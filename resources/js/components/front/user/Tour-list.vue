7b4ee4e5e60b02c720ab00862131a1ce90d4801a
AjayYadavGBI <ajay_yadav@gbinternational.in>
2020-09-21 12:10:54 +0530

    Changes

--- resources/js/components/front/user/Tour-list.vue
+++ resources/js/components/front/user/Tour-list.vue
@@ -33,83 +33,22 @@
         </div>
       </div>
     </div>
+    
     <div v-if="tours">
       <div class="row" v-for="tour in tours" :key="tour.id">
-        <div class="col-md-12">
-          <!-- <div
-            class="bg-cover text-white tour_list_card mt-4"
-            :style="{background-image: linear-gradient(to bottom, rgba(0,0,0,0.6) 0%,rgba(0,0,0,0.6) 100%), `url(${tour.itinerary.detail_photo })}`}"
-          >-->
-          <div
-            class="bg-cover text-white tour_list_card mt-3"
-            :style="{ backgroundImage: `url('/uploadimage/${tour.tour.itinerary.detail_photo}')` }"
-          >
-            <div class="container pt-4 font-weight-bold" v-if="userinfo.information">
-              <div class="row">
-                <div class="col-sm-8">
-                  <p class="font-italic">
-                    <span class="display-4 font-weight-normal">Tour to</span>
-                    <small>({{ tour.tour.tour_start_date }} - {{ tour.tour.tour_end_date }})</small>
-                  </p>
-                  <p class="lead font-weight-normal">{{ tour.tour.itinerary.title }}</p>
-                </div>
 
-                <div
-                  class="col-sm-4 text-center mb-10 mt-5"
-                  v-if="tour.payment == null || tour.payment.status != 'success'"
-                >
-                  <router-link
-                    :to="`/tour-payment/${tour.tour.tour_id}`"
-                    class="btn btn-light"
-                  >PAY NOW</router-link>
-                </div>
-              </div>
-
-              <div class="row text-center" v-if="userinfo.information.user_profession == 'teacher'">
-                <div class="col p-0">
-                  <router-link :to="`/tour-detail/${tour.tour.tour_id}`">
-                    <div class="text-center bg-transparent-card p-t-15 pb-15 text-white">
-                      <i class="fas fa-eye"></i> View Itinerary
-                    </div>
-                  </router-link>
-                </div>
-
-                <div class="col p-0">
-                  <router-link :to="`/add-group/${tour.tour.tour_id}`">
-                    <div class="text-cente bg-transparent-card p-t-15 pb-15 ml-1 text-white">
-                      <i class="fas fa-eye"></i> Add Group
-                    </div>
-                  </router-link>
-                </div>
-                <div class="col p-0">
-                  <router-link :to="`/group-member/${tour.tour.tour_id}`">
-                    <div class="text-cente bg-transparent-card p-t-15 pb-15 ml-1 text-white">
-                      <i class="fas fa-eye"></i> Group Member
-                    </div>
-                  </router-link>
-                </div>
-              </div>
-
-              <div class="row text-center" v-if="userinfo.information.user_profession == 'student'">
-                <div class="col p-0">
-                  <router-link :to="`/tour-detail/${tour.tour.tour_id}`">
-                    <div class="text-center bg-transparent-card p-t-15 pb-15 text-white">
-                      <i class="fas fa-eye"></i> View Itinerary
-                    </div>
-                  </router-link>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <tourcard :tour="tour" :userinfo="userinfo"/>
+        
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import TourCard from '../extra/tour-card';
 export default {
   name: "Tour-list",
+  components:{ tourcard:TourCard},
   data() {
     return {
       tours: [],
